49b15cd76bf7b35a5552538f0bce0a46133b786a
SachkoAlex <aleksandor.sachko@mail.ru>
2020-05-03 17:45:28 +0300

    Challenges lifecycle

--- defenestratio-impl/frontend/src/app/services/challenge.service.ts
+++ defenestratio-impl/frontend/src/app/services/challenge.service.ts
@@ -10,6 +10,8 @@ export class ChallengeService {
   private GET_ALL_CHALLENGES_URI = "/api/challenges";
   private GET_ALL_CHALLENGES_FOR_USER_URI = "/api/challenges/user";
   private CREATE_USER_CHALLENGE_URI = "/api/users/";
+  private UPDATE_CHALLENGE_URL = "/api/challenges/update?";
+  private CREATE_OR_DELETE_CHALLENGE_URI = "/api/challenges?";
 
   private challenges: ChallengeModel[] = [];
 
@@ -24,6 +26,19 @@ export class ChallengeService {
     let body = new HttpParams();
     body = body.set('userId', userId);
     return this.httpClient.get<ChallengeModel[]>(this.GET_ALL_CHALLENGES_FOR_USER_URI, {params: body})
+  }
+
+  createChallenge(challenge: ChallengeModel): Observable<ChallengeModel> {
+    return this.httpClient.post<ChallengeModel>(this.CREATE_OR_DELETE_CHALLENGE_URI,challenge);
+  }
+
+  deleteChallenge(challengeId: string): Observable<any> {
+    return this.httpClient.delete(this.CREATE_OR_DELETE_CHALLENGE_URI + challengeId);
+  }
+
+  updateChallenge(challenge: ChallengeModel): Observable<any> {
+
+    return this.httpClient.post(this.UPDATE_CHALLENGE_URL, challenge);
   }
 
   addUserChallenge(userId: string, challengeId: string) {
